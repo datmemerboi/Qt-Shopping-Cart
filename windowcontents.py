a6aca4548d770bc7a71f02aa4da3b1bf1ba97b16
datmemerboi <shravanssd@gmail.com>
2019-06-22 18:20:04 +0530

    Basic Commit

--- windowcontents.py
+++ windowcontents.py
@@ -34,12 +34,11 @@ class Extender(object):
 		AddButton.move(120, 190)
 		AddButton.clicked.connect(lambda: self.ItemAddFn())
 
-		AddButton = QtWidgets.QPushButton(window)
-		AddButton.setStyleSheet("background-color:white")
-		AddButton.setText("Remove Item")
-		AddButton.move(250, 190)
-		AddButton.clicked.connect(lambda: self.ItemRemFn())
-
+		RemButton = QtWidgets.QPushButton(window)
+		RemButton.setStyleSheet("background-color:white")
+		RemButton.setText("Remove Item")
+		RemButton.move(250, 190)
+		RemButton.clicked.connect(lambda: self.ItemRemFn())
 	def ItemAddFn(self):
 		print("+++")
 		CartItems.append({"Name":self.inputBox.toPlainText().strip(), "Category":self.combos.currentText()})
