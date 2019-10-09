4e71a1f7cebd37b7d238c6c70c85d6aef031c5c7
datmemerboi <shravanssd@gmail.com>
2019-10-09 16:03:10 +0530

    Fifth Commit

--- uiwindow.py
+++ uiwindow.py
@@ -1,7 +1,5 @@
-from PyQt5 import QtWidgets
-
 class UIWindowClass(object):
 	def UIWindowFn(self,window):
 		window.setWindowTitle("UI Window")
-		window.resize(650,600)
+		window.setFixedSize(650, 600)
 		window.setStyleSheet("background-color:rgb(10, 10, 10);")
